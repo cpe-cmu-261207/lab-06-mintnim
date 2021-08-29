59e17747cfaf3adc21f5c9b72d24906f3882497e
mintnim <minttotk@gmail.com>
2021-08-29 16:23:15 +0700

    complete

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,43 +1,38 @@
 import { useState, useEffect } from 'react'
-import { AxiosResponse } from 'axios'
-import { useHistory } from 'react-router-dom'
 import axios from 'axios'
+import { useLocation } from 'react-router'
 
-const Result = () => {
-
-  const [error, seterror] = useState<boolean>(false)
-  const [coin, setcoin] = useState<Record<string,number> | null>(null)
-  let history:any = useHistory().location
-  let a:any = history.state.start
-  let b:any = history.state.end
-
-	useEffect(() => {
-
-		setcoin(null)
-		
-		axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=THB&start=${a}&end=${b}`)
-			.then((resp: AxiosResponse<Record<string,number>>) => {
-				setcoin(resp.data) 
-			})  
-			.catch(err => seterror(true))
-	}, [])
-
-  const createlist = () => {
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+const Result = () =>{
+  let query = useQuery();
+  let start = query.get("start")
+  let end = query.get("end")
+  
+  const [coin,setcoin] = useState<string[]>()
+  const [error, seterror] = useState(false)
+ 
+  useEffect(()=>{
+      axios.get("https://api.coindesk.com/v1/bpi/historical/close.json?currency=THB&start="+start+"&end="+end)
+      .then(resp =>{ 
+          let list = []
+          for (const [key, value] of Object.entries(resp.data.bpi)) {
+              list.push(`${key} - ${Number(value).toLocaleString()} THB`)
+          }
+          setcoin(list)
     
-  }
+      })
+      .catch(err => seterror(true))
+  },[])
 
     return (
         <div className='text-center space-y-3'>
         <p className='text-2xl font-semibold'>Historical price</p>
         {coin === null && error === false ? <p className='text-2xl'>loading ...</p> : <p></p>}
         {error === true ? <p className='text-2xl text-red-500'>There was an error. Please try again later.</p> : <p></p>}
-        {coin === null ? <p></p> : <p className='text-xl font-semibold'> ( From {a} To {b})</p>}
-        {coin === null ? <p></p> : <p className='text-2xl'>{(12).toLocaleString()} THB</p>}
-        <ul>
-          {/* <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li> */}
-        </ul>
+        {coin === null || error === true ? <p></p> : <p className='text-xl font-semibold'> ( From {start} To {end})</p>}
+        {coin?.map(x => <li className='text-xl'>{x} THB</li> )}
       </div>
    
     )
